401291614075f995df604582eec132aafa7357d4
Joshua Rodgers <bytemr@gmail.com>
2025-03-23 11:03:17 -0700

    Add build scripts to produce a godot ready addon.

--- spout-sys/build.rs
+++ spout-sys/build.rs
@@ -1,6 +1,7 @@
 #[cfg(not(target_os = "windows"))]
 fn main() {}
 
+use std::path::Path;
 #[cfg(target_os = "windows")]
 use std::path::PathBuf;
 
@@ -14,6 +15,8 @@ fn main() {
     let include_dir = build_dir.join("include");
     let spout_dx12_include_dir = include_dir.join("SpoutDX12");
 
+    copy_spout_output(&build_dir);
+
     cxx_build::bridge("src/spout.rs")
         .include(spout_dx12_include_dir)
         .include("include/")
@@ -23,12 +26,41 @@ fn main() {
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=spout.rs");
-    println!("cargo:rerun-if-changed=include/library.h");
-    println!("cargo:rerun-if-changed=src/library.cpp");
+    println!("cargo:rerun-if-changed=include/spout.h");
+    println!("cargo:rerun-if-changed=src/spout.cpp");
     println!("cargo:rustc-link-lib=Spout");
     println!("cargo:rustc-link-lib=SpoutDX");
     println!("cargo:rustc-link-lib=SpoutDX12");
     println!("cargo:rustc-link-search=native={}", lib_dir.display());
+}
+
+fn copy_spout_output(build_dir: &Path) {
+    let bin_dir = build_dir.join("bin");
+    let target_dir = get_target_dir();
+
+    let spout_dll_src = bin_dir.join("Spout.dll");
+    let spout_dx_dll_src = bin_dir.join("SpoutDX.dll");
+    let spout_dx12_dll_src = bin_dir.join("SpoutDX12.dll");
+
+    let spout_dll_dst = target_dir.join("Spout.dll");
+    let spout_dx_dll_dst = target_dir.join("SpoutDX.dll");
+    let spout_dx12_dll_dst = target_dir.join("SpoutDX12.dll");
+
+    std::fs::copy(dbg!(spout_dll_src), dbg!(spout_dll_dst)).unwrap();
+    std::fs::copy(spout_dx_dll_src, spout_dx_dll_dst).unwrap();
+    std::fs::copy(spout_dx12_dll_src, spout_dx12_dll_dst).unwrap();
+}
+
+fn get_target_dir() -> PathBuf {
+    let manifest_path = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let profile = std::env::var("PROFILE").unwrap();
+
+    manifest_path
+        .join("..")
+        .join("target")
+        .join(profile)
+        .canonicalize()
+        .unwrap()
 }
 
 #[cfg(target_os = "windows")]
